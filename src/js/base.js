d631190323380379d251af1976af5d6ea9cbbd90
vultorio <68811816+vultorio67@users.noreply.github.com>
2024-01-20 18:06:16 +0100

    lancement du jeu fonctionne avec detecteur de version installé

--- src/js/base.js
+++ src/js/base.js
@@ -10,30 +10,7 @@ const fs = require('fs');
 const axios = require('axios');
 
 const xml2js = require('xml2js');
-
-async function getForgeVersions() {
-  const forgeUrl = 'https://files.minecraftforge.net/maven/net/minecraftforge/forge/maven-metadata.xml';
-
-  try {
-    // Effectuer une requête HTTP pour récupérer le contenu du fichier XML
-    const response = await axios.get(forgeUrl);
-
-    // Utiliser xml2js pour convertir le XML en un objet JavaScript
-    const parser = new xml2js.Parser();
-    const result = await parser.parseStringPromise(response.data);
-
-    console.error(result)
-
-    // Extraire les versions de l'objet JavaScript résultant
-    const forgeVersions = result.metadata.versioning[0].versions[0].version;
-
-    return forgeVersions;
-  } catch (error) {
-    console.error('Erreur lors de la récupération des versions de Forge :', error.message);
-    throw error;
-  }
-}
-
+const { resolve } = require('path');
 
 async function obtenirToutesLesVersions() {
   const url = 'https://launchermeta.mojang.com/mc/game/version_manifest.json';
@@ -64,67 +41,79 @@ async function obtenirToutesLesVersions() {
 
 // Appel de la fonction pour obtenir toutes les versions
 obtenirToutesLesVersions();
+var installGameVersion
 
 
-start.onclick = (e) => {
+start.onclick = async (e) => {
     console.log('-----------------------------')
 
     var gameVersion = versionList.value;
-    const type = typeElement.value;
+    var type = typeElement.value;
 
     if(type=="forge"){
 
-      getTheForgeVersion(gameVersion).then((version) => {
+
+      getTheForgeVersion(gameVersion).then(version => {
+        console.error(version)
+        console.error("::::::::::::::::::::")
         gameVersion = version;
         installGameVersion = gameVersion
-        alert(gameVersion)
         gameVersion = gameVersion.replace('-', '-forge-')
+
+        launch(installGameVersion, gameVersion);
+
       })
-    
+
 
     }else{
       installGameVersion = gameVersion
+      launch(installGameVersion, gameVersion);
     }
-
-    const launcherDir = datastorage.getLauncherDirectory()
-    //console.log(datastorage.getSelectedAccount())
-
-    const selectedAccount = datastorage.getSelectedAccount()
-    var token = ""
-    if(datastorage.getType(selectedAccount) == "microsoft")
-    {
-      token = datastorage.getToken(selectedAccount)
-    }
-    else {
-      token = "default"
-    }
-
-    console.log(token)
-
-    const userName = datastorage.getUserName(selectedAccount)
-    const uuid = datastorage.getuuid(selectedAccount)
-
-    console.log(datastorage.getUserName(selectedAccount))
-
-    console.error(launcherDir+"/versions/"+gameVersion)
-
-    if (fs.existsSync(launcherDir+"/version/"+gameVersion)) {
-      command.launchMinecraft(gameVersion, launcherDir, userName, uuid, token, "default", "2", "3");
-    }
-    else{
-      command.install(installGameVersion, launcherDir, type).then(() => {
-        console.log("start mc")
-        command.launchMinecraft(gameVersion, launcherDir, userName, uuid, token, "default", "2", "3");
-      });
-    }
-
-    //command.launchMinecraft(gameVersion, launcherDir, userName, uuid, token, "default", "2", "3");
-
-    //command.install(gameVersion, launcherDir, type)
-
-    
-    
 }
+
+
+function launch(installGameVersion, gameVersion)
+{
+  var type = typeElement.value;
+  const launcherDir = datastorage.getLauncherDirectory()
+  //console.log(datastorage.getSelectedAccount())
+
+  const selectedAccount = datastorage.getSelectedAccount()
+  var token = ""
+  if(datastorage.getType(selectedAccount) == "microsoft")
+  {
+    token = datastorage.getToken(selectedAccount)
+  }
+  else {
+    token = "default"
+  }
+
+  console.log(token)
+
+  const userName = datastorage.getUserName(selectedAccount)
+  const uuid = datastorage.getuuid(selectedAccount)
+
+  console.log(datastorage.getUserName(selectedAccount))
+
+  const path = `${launcherDir}\\versions\\${gameVersion}`;   
+
+  if (fs.existsSync(path)) {
+    command.launchMinecraft(gameVersion, launcherDir, userName, uuid, token, "default", "2", "3");
+  }
+  else{
+    command.install(installGameVersion, launcherDir, type).then(() => {
+      console.log("start mc")
+     command.launchMinecraft(gameVersion, launcherDir, userName, uuid, token, "default", "2", "3");
+    });
+  }
+
+  //command.launchMinecraft(gameVersion, launcherDir, userName, uuid, token, "default", "2", "3");
+
+  //command.install(gameVersion, launcherDir, type)
+
+}
+
+
 
 function dossierExiste(cheminDuRepertoire, nomDuDossier) {
   try {
@@ -140,31 +129,60 @@ function dossierExiste(cheminDuRepertoire, nomDuDossier) {
   }
 }
 
-
+//une fois la liste des versions trouvé il faut chercher la bonne version
 function getTheForgeVersion(vanillaVersion)
 {
-  getForgeVersions()
-      .then((versions) => {
-        console.log('Versions de Forge :', versions);
-        
-        var elementContenant121 = null;
-        for (var i = 0; i < versions.length; i++) {
-            if (versions[i].includes(vanillaVersion)) {
-                elementContenant121 = versions[i];
-                break; // Sortir de la boucle dès que l'élément est trouvé
-            }
-        }
-        
-        // Vérifier si un élément a été trouvé
-        if (elementContenant121 !== null) {
-            return elementContenant121
-        } else {
-            return("error")
-        }
+  return new Promise((resolve, reject) => {
+    // Appeler la première fonction asynchrone
+    getForgeVersions(vanillaVersion).then(versions => {
+      console.log('Versions de Forge :', versions);
+      var versionForge = null;
+      for (var i = 0; i < versions.length; i++) {
+          var mcVersion = versions[i].split("-")[0]
+          if (mcVersion.includes(vanillaVersion)) {
+            versionForge = versions[i];
+              break; // Sortir de la boucle dès que l'élément est trouvé
+          }
+      }
+      
+      // Vérifier si un élément a été trouvé
+      if (versionForge !== null) {
+          resolve( versionForge)
+      } else {
+          resolve("error")
+      }
+    }).catch(error => {
+        reject(error); // En cas d'erreur dans la première fonction, rejeter la promesse
+    });
+});
 
-
-      })
-      .catch((error) => {
-        console.error('Erreur globale :', error.message);
-      });
 }
+
+
+//cherche la liste des version
+async function getForgeVersions() {
+  return new Promise(async (resolve, reject) => {
+    const forgeUrl = 'https://files.minecraftforge.net/maven/net/minecraftforge/forge/maven-metadata.xml';
+
+    try {
+      // Effectuer une requête HTTP pour récupérer le contenu du fichier XML
+      const response = await axios.get(forgeUrl);
+  
+      // Utiliser xml2js pour convertir le XML en un objet JavaScript
+      const parser = new xml2js.Parser();
+      const result = await parser.parseStringPromise(response.data);
+  
+      console.error(result)
+  
+      // Extraire les versions de l'objet JavaScript résultant
+      const forgeVersions = result.metadata.versioning[0].versions[0].version;
+  
+      resolve( forgeVersions);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des versions de Forge :', error.message);
+      throw error;
+    }
+});
+ 
+}
+
